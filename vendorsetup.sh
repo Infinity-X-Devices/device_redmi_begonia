0d455c7be1cd3745bc007179afbf50d3387f5c8d
ShantanuPC <shantanuchimote42@gmail.com>
2025-04-28 02:52:41 +0530

    begonia: Add vendorsetup.sh
    Signed-off-by: ShantanuPC <shantanuchimote42@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+git clone --depth 1 https://github.com/shantanupc/device_redmi_begonia -b 15 device/redmi/begonia
+git clone --depth 1 https://github.com/shantanupc/vendor_redmi_begonia vendor/redmi/begonia
+git clone --depth 1 https://github.com/Saikrishna1504/vendor_redmi_begonia-ims vendor/redmi/begonia-ims
+git clone --depth 1 https://github.com/Saikrishna1504/vendor_redmi_begonia-firmware vendor/redmi/begonia-firmware
+git clone --depth 1 https://github.com/Saikrishna1504/device_mediatek_sepolicy device/mediatek/sepolicy
+git clone --depth 1 https://github.com/Saikrishna1504/hardware_mediatek -b vic hardware/mediatek
+git clone --depth 1 https://github.com/saikrishna1504/kernel_xiaomi_mt6785 -b vic kernel/xiaomi/mt6785
+git clone --depth 1 https://bitbucket.org/saikrishna1504/vendor_miuicameraleica vendor/MiuiCameraLeica
+git clone --depth 1 https://github.com/begonia-dev/android_packages_apps_MtkFMRadio packages/apps/MtkFMRadio
